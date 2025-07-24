1238ec2f591d1714b9583db79be20a4a749770df
jakiulhasan <mdjakiulhasan17@gmail.com>
2025-07-24 14:41:11 +0600

    badge shadow added & some adjustment

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const addToCart = document.querySelectorAll(".add-to-cart");
 
 addToCart.forEach(function (addToCart) {
   addToCart.addEventListener("click", function () {
-    alert("Product Added To Cart");
+    alert("Flower Added to Cart");
   });
 });
